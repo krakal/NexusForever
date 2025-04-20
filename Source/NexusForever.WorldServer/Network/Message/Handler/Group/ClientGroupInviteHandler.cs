7505c2e370f7a83478503e315a51f6666e88b6ad
krakal <krakaldev@gmail.com>
2025-04-20 19:45:50 +0100

    Made it possible for inviter to be in Instance group or Party group.

--- Source/NexusForever.WorldServer/Network/Message/Handler/Group/ClientGroupInviteHandler.cs
+++ Source/NexusForever.WorldServer/Network/Message/Handler/Group/ClientGroupInviteHandler.cs
@@ -14,65 +14,78 @@ namespace NexusForever.WorldServer.Network.Message.Handler.Group
         /// </summary>
         public void HandleMessage(IWorldSession session, ClientGroupInvite groupInvite)
         {
-            IPlayer targetedPlayer = PlayerManager.Instance.GetPlayer(groupInvite.InviteeName);
-            if (targetedPlayer == null)
+            IPlayer inviter = session.Player;
+            IPlayer invitee = PlayerManager.Instance.GetPlayer(groupInvite.InviteeName);
+
+            if (invitee == null)
             {
                 GroupHandler.SendGroupResult(session, GroupResult.PlayerNotFound, targetPlayerName: groupInvite.InviteeName);
                 return;
             }
 
             // Check if targeted player is already grouped in an instance Group they cannot be re-invited, only instance finder can create an instance group.
-            if (targetedPlayer.GroupMembershipInstance != null)
+            if (invitee.GroupMembershipInstance != null)
             {
                 GroupHandler.SendGroupResult(session, GroupResult.Grouped, targetPlayerName: groupInvite.InviteeName);
                 return;
             }
 
             // Check if inviter faction is same as invited faction.
-            if (targetedPlayer.Faction1 != session.Player.Faction1)
+            if (invitee.Faction1 != session.Player.Faction1)
             {
                 GroupHandler.SendGroupResult(session, GroupResult.WrongFaction, targetPlayerName: groupInvite.InviteeName);
                 return;
             }
 
             // Player is already being invited by another group/player
-            if (targetedPlayer.GroupInvite != null)
+            if (invitee.GroupInvite != null)
             {
                 GroupHandler.SendGroupResult(session, GroupResult.Pending, targetPlayerName: groupInvite.InviteeName);
                 return;
             }
 
             // Check if the inviter is not inviting himself (pleb)
-            if (targetedPlayer.Session == session)
+            if (invitee.Session == session)
             {
                 GroupHandler.SendGroupResult(session, GroupResult.NotInvitingSelf, targetPlayerName: groupInvite.InviteeName);
                 return;
             }
 
-            // TODO(krakal): Add function that finds player amongst all groups. Just because player.GroupMembership is null does not mean they are not in a group.
-            if (session.Player.GroupMembershipInstance == null)
+            if (inviter.GroupMembershipInstance == null && inviter.GroupMembershipParty == null)
             {
-                // Player is not part of a group - lets create a new one and invite the new guy.
-                IGroup newGroup = GroupManager.Instance.CreateGroup(session.Player);
-                newGroup.Invite(session.Player, targetedPlayer);
+                // Inviter is not part of a group - lets create a new one and invite the new guy.
+                IGroup newGroup = GroupManager.Instance.CreatePartyGroup(leader: inviter);
+                newGroup.Invite(inviter, invitee);
                 return;
             }
-
-            // At this point, the player must be part of a group
-            IGroup group = session.Player.GroupMembershipInstance.Group;
-            IGroupMember membership = session.Player.GroupMembershipInstance;
-
-            if (group.IsFull)
+            else
             {
-                GroupHandler.SendGroupResult(session, GroupResult.Full, group.Id, groupInvite.InviteeName);
-                return;
-            }
+                IGroup group = null;
+                IGroupMember inviterMember = null;
 
-            // The inviter is the Leader or has Invite permissions, so just do an invite.
-            if (group.Leader.Id == membership.Id || membership.Flags.HasFlag(GroupMemberInfoFlags.CanInvite))
-                group.Invite(session.Player, targetedPlayer);
-            else // inviter is another group memeber w/o invite permissions, so we create a referal.
-                group.ReferMember(membership, targetedPlayer);
+                if (inviter.GroupMembershipInstance != null)
+                {
+                    group = inviter.GroupMembershipInstance.Group;
+                    inviterMember = inviter.GroupMembershipInstance;
+                }
+                else
+                {
+                    group = inviter.GroupMembershipParty.Group;
+                    inviterMember = inviter.GroupMembershipParty;
+                }
+                
+                if (group.IsFull)
+                {
+                    GroupHandler.SendGroupResult(session, GroupResult.Full, group.Id, groupInvite.InviteeName);
+                    return;
+                }
+
+                // The inviter is the Leader or has Invite permissions, so just do an invite.
+                if (group.Leader.Id == inviterMember.Id || inviterMember.Flags.HasFlag(GroupMemberInfoFlags.CanInvite))
+                    group.Invite(inviter, invitee);
+                else // inviter is another group memeber w/o invite permissions, so we create a referal.
+                    group.ReferMember(inviterMember, invitee);
+            }
         }
     }
 }
