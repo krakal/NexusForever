7c95822c50f08b1b1be98735e035792b7e346b0c
krakal <krakaldev@gmail.com>
2025-04-21 12:46:37 +0100

    Added ability to ping maps in group.

---
+++ Source/NexusForever.WorldServer/Network/Message/Handler/Group/ClientZoneMapPingHandler.cs
@@ -0,0 +1,35 @@
+﻿using NexusForever.Game.Abstract.Entity;
+using NexusForever.Game.Abstract.Group;
+using NexusForever.Network.Message;
+using NexusForever.Network.World.Message.Model;
+
+namespace NexusForever.WorldServer.Network.Message.Handler.Group
+{
+    public class ClientZoneMapPingHandler : IMessageHandler<IWorldSession, ClientZoneMapPing>
+    {
+        public void HandleMessage(IWorldSession session, ClientZoneMapPing clientPing)
+        {
+            IPlayer pingMaker = session.Player;
+            IGroup group = null;
+            
+            if (pingMaker.GroupMembershipForeground != null)
+            {
+                group = pingMaker.GroupMembershipForeground.Group;
+            }
+            else if (pingMaker.GroupMembershipBackground != null)
+            {
+                group = pingMaker.GroupMembershipBackground.Group;
+            }
+            else
+            {
+                return;
+            }
+
+            group.BroadcastPacket(new ServerZoneMapPing
+            {
+                Invoker = clientPing.Invoker,
+                PingLocation = clientPing.PingLocation
+            });
+        }
+    }
+}
