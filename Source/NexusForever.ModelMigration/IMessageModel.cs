3edf728fc31c0eb719cbb2d2e1d177740dfccf61
Neiko <rawahodev@gmail.com>
2025-04-21 13:54:09 +1200

    * A few examples of making a data model seperate from the message and game logic. The classes in the NexusForever.ModelMigration project/namespace could me moved anywhere deemed logical to be a common dependency.

---
+++ Source/NexusForever.ModelMigration/IMessageModel.cs
@@ -0,0 +1,6 @@
+﻿namespace NexusForever.ModelMigration
+{
+    public interface IMessageModel
+    {
+    }
+}
--- Source/NexusForever.WorldServer/Network/Message/Handler/Entity/ClientActivateUnitHandler.cs
+++ Source/NexusForever.WorldServer/Network/Message/Handler/Entity/ClientActivateUnitHandler.cs
@@ -1,13 +1,13 @@
 ﻿using NexusForever.Game.Abstract.Entity;
+using NexusForever.ModelMigration;
 using NexusForever.Network;
 using NexusForever.Network.Message;
-using NexusForever.Network.World.Message.Model;
 
 namespace NexusForever.WorldServer.Network.Message.Handler.Entity
 {
-    public class ClientActivateUnitHandler : IMessageHandler<IWorldSession, ClientActivateUnit>
+    public class ClientActivateUnitHandler : IMessageHandler<IWorldSession, ClientActivateUnitModel>
     {
-        public void HandleMessage(IWorldSession session, ClientActivateUnit activateUnit)
+        public void HandleMessage(IWorldSession session, ClientActivateUnitModel activateUnit)
         {
             IWorldEntity entity = session.Player.GetVisible<IWorldEntity>(activateUnit.UnitId);
             if (entity == null)
